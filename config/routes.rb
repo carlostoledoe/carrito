9728a5f41c5837576c88b3e558da320da1bc24d6
Carlos Toledo <carlombo@hotmail.com>
2022-10-23 21:03:43 -0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'cart/show'
+  get 'cart', to: 'cart#show'
+  post 'cart/add'
+  post 'cart/remove'
   resources :products
+  root 'products#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
