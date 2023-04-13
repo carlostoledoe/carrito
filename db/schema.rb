17976cd3a7ae4c0ea92b39fe9b1e1f4616243b44
Carlos Toledo <carlombo@hotmail.com>
2023-04-13 19:43:58 -0400

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_23_234437) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_13_233937) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -32,7 +32,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_23_234437) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "descrption"
-    t.decimal "price", precision: 5, scale: 2
+    t.decimal "price", precision: 10, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
